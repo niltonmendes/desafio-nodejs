c2b2b474fc78f31f6b40b2e7feaa2ebe7013b42e
Nilton Mendonca Junior <niltonmendonca@MacBook-Pro-de-Nilton.local>
2020-05-26 02:40:29 -0300

    Add like repository function

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const uniqid = require("uniqid");
+
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -9,14 +10,33 @@ app.use(cors());
 
 const repositories = [];
 
+function validateRepositoryId(request, response, next) {
+  const { id } = request.param;
+
+  if(!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid repository ID' });
+  }
+
+  return next();
+}
+
+app.use('/repositories/:id', validateRepositoryId);
+
 app.get("/repositories", (request, response) => {
   return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  const { title, url, techs, likes = 0 } = request.body;
+  const { title, url, techs } = request.body;
 
-  const repository = { id: uniqid(), title, url, techs, likes };
+  const repository = { 
+    id: uuid(), 
+    title, 
+    url, 
+    techs, 
+    likes: 0,
+  };
+
   repositories.push(repository);
 
   return response.json(repository);
@@ -38,6 +58,7 @@ app.put("/repositories/:id", (request, response) => {
     title,
     url,
     techs,
+    likes: repositories[repositoryIndex].likes,
   };
 
   repositories[repositoryIndex] = repository;
@@ -61,7 +82,17 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex((repository) => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found' });
+  }
+
+  repositories[repositoryIndex].likes =
+    (Number(repositories[repositoryIndex].likes) || 0) + 1;
+
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
